083e97c6992a70287d4799396bda2e4fc805eabb
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-01-27 16:11:10 +0100

    [add] about + contacts + fix bg

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -5,9 +5,17 @@
 </script>
 
 <template>
-    <h1>
-        ABOUT
-    </h1>
+    <main>
+        <div class="container text-center fw-bold text-white py-4">
+            <h1>
+                CHI SONO
+            </h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ea nulla sapiente, corrupti sint architecto impedit! At distinctio praesentium voluptatibus eum dolore, tempore facilis vel repellat dolores porro explicabo aliquam!</p>
+        </div>
+        
+    </main>
+    
 </template>
 
 
--- resources/js/pages/Contacts.vue
+++ resources/js/pages/Contacts.vue
@@ -5,9 +5,16 @@
 </script>
 
 <template>
-    <h1>
-        Contacts
-    </h1>
+        <main>
+        <div class="container text-center fw-bold text-white py-4">
+            <h1>
+                CONTATTI
+            </h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ea nulla sapiente, corrupti sint architecto impedit! At distinctio praesentium voluptatibus eum dolore, tempore facilis vel repellat dolores porro explicabo aliquam!</p>
+        </div>
+        
+    </main>
 </template>
 
 
